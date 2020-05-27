0eaea0457ddba0bc1ad013215a04bf116b292d95
Ritabrata Maiti <ritabratamaiti@gmail.com>
2020-05-28 01:51:26 +0530

    Update Chem_Faiss_flask.py

--- Chem_Faiss_flask.py
+++ Chem_Faiss_flask.py
@@ -2,14 +2,19 @@ from flask import Flask, jsonify, request
 from Chem_Faiss import pipeline
 import Chem_Faiss
 
+
 app = Flask(__name__)
 searcher = pipeline()
 searcher.load_pipeline('sample')
 mols = Chem_Faiss.load_sdf('molecules.sdf')
 
-@app.route('/query', method = 'POST')
+
+@app.route('/query', methods = ['GET','POST'])
 def query():
-    q = request.form('SMILES')
+    d = request.get_json()
+    s = d['SMILES']
+    print(s)
+    print(request.form)
     I = searcher.make_query_smiles(q = s, k = 2)
     smiles = Chem_Faiss.idx_to_smiles(mols, I)
     return jsonify({
